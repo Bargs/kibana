e4739c6f30e861ff6df78419af63f24f63cf7e3f
Matthew Bargar <mbargar@gmail.com>
2017-09-19 15:59:10 -0400

    saving and loading queries working

--- src/ui/public/query_bar/directive/query_bar.js
+++ src/ui/public/query_bar/directive/query_bar.js
@@ -3,6 +3,7 @@ import { callAfterBindingsWorkaround } from 'ui/compat';
 import template from './query_bar.html';
 import { queryLanguages } from '../lib/queryLanguages';
 import { documentationLinks } from '../../documentation_links/documentation_links.js';
+import { SavedObjectsClientProvider } from '../../saved_objects';
 
 const module = uiModules.get('kibana');
 
@@ -19,13 +20,24 @@ module.directive('queryBar', function () {
     },
     controllerAs: 'queryBar',
     bindToController: true,
-    controller: callAfterBindingsWorkaround(function ($scope, config) {
+    controller: callAfterBindingsWorkaround(function ($scope, config, Private) {
+      const savedObjectsClient = Private(SavedObjectsClientProvider);
       this.queryDocLinks = documentationLinks.query;
       this.appName = this.appName || 'global';
       this.availableQueryLanguages = queryLanguages;
       this.showLanguageSwitcher = config.get('search:queryLanguage:switcher:enable');
       this.typeaheadKey = () => `${this.appName}-${this.query.language}`;
       this.showSaveLoad = false;
+
+      const getSavedQueries = () => {
+        savedObjectsClient.find({
+          type: 'query',
+          perPage: 10,
+          search: `*`,
+        }).then(response => {
+          this.savedQueries = response.savedObjects;
+        });
+      };
 
       this.submit = () => {
         this.onSubmit({ $query: this.localQuery });
@@ -40,9 +52,23 @@ module.directive('queryBar', function () {
         this.showSaveLoad = !this.showSaveLoad;
       };
 
+      this.saveQuery = () => {
+        const attributes = Object.assign({}, this.localQuery, { name: this.saveQueryName });
+
+        savedObjectsClient.create('query', attributes)
+        .then(getSavedQueries);
+      };
+
+      this.loadQuery = (savedQuery) => {
+        this.localQuery = savedQuery.attributes;
+        this.submit();
+      };
+
       $scope.$watch('queryBar.query', (newQuery) => {
         this.localQuery = Object.assign({}, newQuery);
       }, true);
+
+      getSavedQueries();
     })
   };
 
